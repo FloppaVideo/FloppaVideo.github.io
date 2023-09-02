f113a2dcd037b9290c1db2a4cf25dfddc0a0eaf8
Floppa God <93928325+JJCE-Developer@users.noreply.github.com>
2023-09-02 12:54:48 -0700

    It's a little more secure now

--- js/AccountHandler.js
+++ js/AccountHandler.js
@@ -1,7 +1,7 @@
 const name = document.getElementById('name').value;
 const email = document.getElementById('email').value;
-const password = document.getElementById('pass').value;
+//const password = document.getElementById('pass').value;
 
 document.cookie = "user=" +name+ "; max_age=604800";
 document.cookie = "email=" +email+ "; max_age=604800";
-document.cookie = "password=" +password+ "; max_age=604800";
+//document.cookie = "password=" +password+ "; max_age=604800";
