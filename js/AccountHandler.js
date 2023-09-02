d8627935393b6949afec85d2869d6ee2c8b37364
Floppa God <93928325+JJCE-Developer@users.noreply.github.com>
2023-09-02 12:04:31 -0700

    Update AccountHandler.js

--- js/AccountHandler.js
+++ js/AccountHandler.js
@@ -1,6 +1,7 @@
 var username;
 var userID;
 
-//function saveCookie(){
-//  document.createElement
-//}
+function saveCookie(){
+  //Login cookie
+  document.cookie = "user=userID; max_age=604800";
+}
